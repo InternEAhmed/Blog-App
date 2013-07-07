881c880ff5386cf5329aceb4fba54be0879dff00
internEAhmed <intern.eahmed@gmail.com>
2013-07-07 10:37:42 +0200

    Fixed HTML files

--- Blog/blogapp/views.py
+++ Blog/blogapp/views.py
@@ -5,11 +5,11 @@ def index(request):
     # get the blog posts that are published
     posts = Post.objects.filter(published=True)
     # now return the rendered template
-    return render(request, 'blogapp/index.html', {'posts': posts})
+    return render(request, 'index.html', {'posts': posts})
 
 def post(request, slug):
     # get the Post object
     post = get_object_or_404(Post, slug=slug)
     # now return the rendered template
-    return render(request, 'blogapp/post.html', {'post': post})
+    return render(request, 'post.html', {'post': post})
 
